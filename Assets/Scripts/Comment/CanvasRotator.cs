412868147a1c955e35228866225b5c51712c8a36
ShimamotoWONQ <shimamoto@wonq.co.jp>
2026-01-26 17:55:49 +0900

    🔧 update: improve performance in Comment domain

--- Assets/Scripts/Comment/CanvasRotator.cs
+++ Assets/Scripts/Comment/CanvasRotator.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class CanvasRotator : MonoBehaviour
 {
+    const float PositionChangeThreshold = 0.001f;
 
     Camera targetCamera;
+    Vector3 lastCameraPosition;
+    Quaternion lastCameraRotation;
 
     enum LockAxis
     {
@@ -18,20 +21,43 @@ public class CanvasRotator : MonoBehaviour
     public void Init(Camera targetCamera)
     {
         this.targetCamera = targetCamera;
+        UpdateRotation();
     }
 
     void Update()
     {
-        Vector3 CamToComment = transform.position - targetCamera.transform.position;
+        if (!HasCameraTransformChanged()) return;
 
-        Vector3 locked = lockAxis switch
+        UpdateRotation();
+    }
+
+    bool HasCameraTransformChanged()
+    {
+        Transform camTransform = targetCamera.transform;
+        bool hasChanged = (camTransform.position - lastCameraPosition).sqrMagnitude > PositionChangeThreshold
+                       || camTransform.rotation != lastCameraRotation;
+
+        if (hasChanged)
         {
-            LockAxis.None => CamToComment,
-            LockAxis.X => new Vector3(0.0f, CamToComment.y, CamToComment.z),
-            LockAxis.Y => new Vector3(CamToComment.x, 0.0f, CamToComment.z),
+            lastCameraPosition = camTransform.position;
+            lastCameraRotation = camTransform.rotation;
+        }
+
+        return hasChanged;
+    }
+
+    void UpdateRotation()
+    {
+        Vector3 directionToCamera = transform.position - targetCamera.transform.position;
+
+        Vector3 lockedDirection = lockAxis switch
+        {
+            LockAxis.None => directionToCamera,
+            LockAxis.X => new Vector3(0f, directionToCamera.y, directionToCamera.z),
+            LockAxis.Y => new Vector3(directionToCamera.x, 0f, directionToCamera.z),
             _ => throw new ArgumentOutOfRangeException()
         };
 
-        transform.rotation = Quaternion.LookRotation(locked);
+        transform.rotation = Quaternion.LookRotation(lockedDirection);
     }
 }
